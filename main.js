f73a3270b905d2b0c0c608deb6f41423773b9696
Jasper <jpmorgal8@gmail.com>
2019-05-28 19:32:29 -0700

    Put in percent time

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-function changeTime() {
-    var date = new Date()
-    document.getElementById("time").innerHTML = date;
+function updateTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var hoursday = hours / 24;
+
+    var minutes = date.getMinutes();
+    var minutesday = minutes / 1440;
+
+    var seconds = date.getSeconds();
+    var secondsday = seconds / 86400;
+
+    var decimal = hoursday + minutesday + secondsday;
+    var rounded = Math.round(decimal * 100000) / 1000;
+    var string = rounded.toString() + '%';
+    document.getElementById("time").innerHTML = string;
 }
 
-window.onload = changeTime
-window.setInterval(changeTime, 1)
+window.onload = updateTime;
+window.setInterval(updateTime, 1);
