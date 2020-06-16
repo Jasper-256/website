66d27481ea7e8b98f5639fa86c57efb63515f6d9
Jasper Morgal <jpmorgal8@gmail.com>
2020-06-16 16:29:44 -0700

    Added other ships that shoot at you to space game.

--- space.js
+++ space.js
@@ -1,10 +1,11 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-var redSquare;
 var xpCounter;
-var myObstacles = [];
+var player;
+var enemyShips = [];
 var bullets = [];
+var food = [];
 
 
 // Changeable charicteristics to customize the game:
@@ -13,16 +14,23 @@ var xpOffset = 40;
 
 var playerWidth = 50;
 var playerHeight = 20;
-var movementSpeed = 3;
+var playerSpeed = 3;
+var autoSpinSpeed = 1.5; // 4 secs per rotation
 
-var bulletSpeed = 10;
-var bulletRate = 2;
+var bulletSpeed = 8;
+var bulletRate = 4; // 15 per sec (every 4 frames)
 var bulletVariationDegrees = 15;
 var maxBulletLifetime = 180; // 3 secs
 
-var minEnemySize = 10;
-var maxEnemySize = 20;
-var maxEnemyNo = 2000;
+var enemyShipNo = 10;
+var enemyShipSpeed = 2;
+var enemyShipLockOnDistance = 700;
+var enemyShipStopDistance = 400;
+var enemyShipBackUpDistance = 100;
+
+var minFoodSize = 10;
+var maxFoodSize = 20;
+var foodNo = 2000;
 
 var mapSize = 4000;
 // End of changeable charicterestics
@@ -31,9 +39,9 @@ var mapSize = 4000;
 var xp = 0;
 var level = 0;
 
-var currentEnemyNo = 0;
-var bulletNumber = 0;
-var started = false;
+var currentEnemyShipNo = 0;
+var currentFoodNo = 0;
+// var started = false;
 
 var mouseDown = false;
 
@@ -81,7 +89,7 @@ document.addEventListener('keyup', function(e) {
 function startGame() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    redSquare = new component(playerWidth, playerHeight, "red", canvas.width / 2, canvas.height / 2);
+    player = new component(playerWidth, playerHeight, "red", canvas.width / 2, canvas.height / 2);
     xpCounter = new component("30px", "Arial", "black", 0, 0, "text"); // x and y for this are set in updateGameArea()
 }
 
@@ -91,6 +99,8 @@ function clear() {
 
 
 startGame();
+
+
 
 
 
@@ -107,6 +117,7 @@ function component(width, height, color, x, y, type) {
     this.speedX = 0;
     this.speedY = 0;
     this.lifetime = 0;
+    this.bulletNumber = 0;
     
     this.update = function() {
         ctx = context;
@@ -126,16 +137,12 @@ function component(width, height, color, x, y, type) {
         }
     }
 
-    this.updateBullets = function() {
-        ctx = context;
-
-
+    this.moveBasedOnSpeed = function() {
+        this.x += this.speed * Math.cos(this.angle);
+        this.y += this.speed * Math.sin(this.angle);
     }
 
     this.newPos = function() {
-        this.x += this.speed * Math.cos(this.angle);
-        this.y += this.speed * Math.sin(this.angle);
-
         x = -(this.speed * Math.cos(this.angle));
         y = -(this.speed * Math.sin(this.angle));
         translateX -= x;
@@ -174,20 +181,58 @@ function component(width, height, color, x, y, type) {
 
 
 
+
 function updateGameArea() {
 
-    for (i = 0; i < myObstacles.length; i += 1) {
+    function toRadians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    function toDegrees(radians) {
+        return radians * 180 / Math.PI;
+    }
+
+    function angle(cx, cy, ex, ey) {
+        var dy = ey - cy;
+        var dx = ex - cx;
+        var theta = Math.atan2(dy, dx); // range (-PI, PI]
+        theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+        // if (theta < 0) theta = 360 + theta; // range [0, 360)
+        return theta;
+    }
+
+    function distance(x1, y1, x2, y2) {
+        return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+    }
+
+
+
+
+    for (f = 0; f < food.length; f += 1) {
         for (b = 0; b < bullets.length; b += 1) {
-            if (bullets[b].crashWith(myObstacles[i])) {
-                myObstacles.splice(i, 1);
+            if (bullets[b].crashWith(food[f])) {
+                food.splice(f, 1);
                 bullets.splice(b, 1);
-                currentEnemyNo -= 1;
+                currentFoodNo -= 1;
                 xp += 1;
             }
         }
     }
 
+    for (a = 0; a < bullets.length; a += 1) {
+        for (b = 0; b < bullets.length; b += 1) {
+            if (a != b) {
+                if (bullets[a].crashWith(bullets[b])) {
+                    bullets.splice(a, 1);
+                    bullets.splice(b, 1);
+                }
+            }
+        }
+    }
+
     clear();
+
+    
 
     if (mouseX && mouseY) {
 
@@ -206,43 +251,18 @@ function updateGameArea() {
         }
 
 
-        function toRadians(degrees) {
-            // try {
-            return degrees * Math.PI / 180;
-            // } catch(error) {
-            //     console.log("toRadians() function error: " + error);
-            //     return 0;
-            // }
-        }
-
-        function toDegrees(radians) {
-            return radians * 180 / Math.PI;
-        }
-
-
-        function angle(cx, cy, ex, ey) {
-            var dy = ey - cy;
-            var dx = ex - cx;
-            var theta = Math.atan2(dy, dx); // range (-PI, PI]
-            theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
-            // if (theta < 0) theta = 360 + theta; // range [0, 360)
-            return theta;
-        }
-
-
-        distance = Math.sqrt((mouseCenterX * mouseCenterX) + (mouseCenterY * mouseCenterY));
-        // console.log(distance);
+        var mouseDistanceFromPlayer = distance(mouseCenterX, mouseCenterY, 0, 0);
         
         if (!autospin) {
-            redSquare.angle = toRadians(angle(0, 0, mouseCenterX, mouseCenterY));
+            player.angle = toRadians(angle(0, 0, mouseCenterX, mouseCenterY));
         } else {
-            redSquare.angle += toRadians(1);
+            player.angle += toRadians(autoSpinSpeed);
         }
 
-        if (distance >= mouseDeadZone) {
-            redSquare.speed = movementSpeed;
+        if (mouseDistanceFromPlayer >= mouseDeadZone) {
+            player.speed = playerSpeed;
         } else {
-            redSquare.speed = 0;
+            player.speed = 0;
         }
     }
 
@@ -250,12 +270,7 @@ function updateGameArea() {
     var centerX = translateX + (canvas.width / 2);
     var centerY = translateY + (canvas.height / 2);
 
-    if (redSquare.angle != 0) {
-        started = true;
-    }
-    if (redSquare.speed != 0) {
-        started = true;
-    }
+
 
     if (keys && keys[69] && !autofireKeyDown) {
         autofire = !autofire;
@@ -267,22 +282,18 @@ function updateGameArea() {
     }
 
     if ((keys && keys[32]) || mouseDown || autofire) { // space pressed
-        if (bulletNumber % bulletRate == 0) {
-            if ((redSquare.angle != 0) || started) {
-                bullets.push(new component(5, 5, "blue", centerX, centerY));
+        if (player.bulletNumber % bulletRate == 0) {
+            bullets.push(new component(5, 5, "blue", centerX, centerY));
 
-                var shootAngle = redSquare.angle;
-                try {
-                    shootAngle += toRadians((Math.random() * bulletVariationDegrees) - (bulletVariationDegrees / 2));
-                } catch(error) {
-                    console.log("toRadians() function calling error: " + error);
-                }
-                bullets[bullets.length - 1].speedX += bulletSpeed * Math.cos(shootAngle);
-                bullets[bullets.length - 1].speedY += bulletSpeed * Math.sin(shootAngle);
-            }
+            var shootAngle = player.angle;
+            shootAngle += toRadians((Math.random() * bulletVariationDegrees) - (bulletVariationDegrees / 2));
+
+            bullets[bullets.length - 1].speedX += bulletSpeed * Math.cos(shootAngle);
+            bullets[bullets.length - 1].speedY += bulletSpeed * Math.sin(shootAngle);
         }
-        bulletNumber += 1;
+        player.bulletNumber += 1;
     }
+
 
     for (i = 0; i < bullets.length; i += 1) {
         // bullets[i].x += 8;
@@ -294,28 +305,66 @@ function updateGameArea() {
         }
     }
 
-
-
-    while (currentEnemyNo < maxEnemyNo) {
+    function getRandomX() {
         var mapXMin = (canvas.width / 2) - (mapSize / 2);
         var mapXMax = (canvas.width / 2) + (mapSize / 2);
-        var randomX = Math.floor(Math.random() * (mapXMax - mapXMin)) + mapXMin;
+        return randomX = Math.floor(Math.random() * (mapXMax - mapXMin)) + mapXMin;
+    }
 
+    function getRandomY() {
         var mapYMin = (canvas.height / 2) - (mapSize / 2);
         var mapYMax = (canvas.height / 2) + (mapSize / 2);
-        var randomY = Math.floor(Math.random() * (mapYMax - mapYMin)) + mapYMin;
-
-        var randomSize = Math.floor(Math.random() * (maxEnemySize - minEnemySize)) + minEnemySize;
-
-        myObstacles.push(new component(randomSize, randomSize, "green", randomX, randomY));
-
-        currentEnemyNo += 1;
+        return randomY = Math.floor(Math.random() * (mapYMax - mapYMin)) + mapYMin;
     }
 
 
-    for (i = 0; i < myObstacles.length; i += 1) {
+    while (currentFoodNo < foodNo) {
+        var randomSize = Math.floor(Math.random() * (maxFoodSize - minFoodSize)) + minFoodSize;
+
+        food.push(new component(randomSize, randomSize, "green", getRandomX(), getRandomY()));
+
+        currentFoodNo += 1;
+    }
+
+    while (currentEnemyShipNo < enemyShipNo) {
+        enemyShips.push(new component(playerWidth, playerHeight, "purple", getRandomX(), getRandomY()));
+
+        currentEnemyShipNo += 1;
+    }
+
+
+    for (i = 0; i < food.length; i += 1) {
         // myObstacles[i].x += -1;
-        myObstacles[i].update();
+        food[i].update();
+    }
+
+    for (i = 0; i < enemyShips.length; i += 1) {
+
+        var distanceToPlayer = distance(enemyShips[i].x, enemyShips[i].y, player.x, player.y);
+
+        enemyShips[i].angle = toRadians(angle(enemyShips[i].x, enemyShips[i].y, player.x, player.y));
+
+        if (distanceToPlayer < enemyShipLockOnDistance && distanceToPlayer > enemyShipStopDistance) {
+            enemyShips[i].speed = enemyShipSpeed;
+        } else if (distanceToPlayer < enemyShipBackUpDistance) {
+            enemyShips[i].speed = -enemyShipSpeed;
+        } else {
+            enemyShips[i].speed = 0;
+        }
+
+        if (distanceToPlayer < enemyShipLockOnDistance && enemyShips[i].bulletNumber % bulletRate == 0) {
+            bullets.push(new component(5, 5, "blue", enemyShips[i].x, enemyShips[i].y));
+
+            var shootAngle = enemyShips[i].angle;
+            shootAngle += toRadians((Math.random() * bulletVariationDegrees) - (bulletVariationDegrees / 2));
+
+            bullets[bullets.length - 1].speedX += bulletSpeed * Math.cos(shootAngle);
+            bullets[bullets.length - 1].speedY += bulletSpeed * Math.sin(shootAngle);
+        }
+
+        enemyShips[i].bulletNumber += 1;
+        enemyShips[i].moveBasedOnSpeed();
+        enemyShips[i].update();
     }
 
     xpCounter.text = "XP: " + xp;
@@ -323,7 +372,8 @@ function updateGameArea() {
     xpCounter.y = canvas.height - xpOffset + translateY;
     xpCounter.update();
 
-    redSquare.newPos();
-    redSquare.update();
+    player.moveBasedOnSpeed();
+    player.newPos();
+    player.update();
 }
 
