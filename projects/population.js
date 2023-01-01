44fa7e7c2bbabf1eaa7d2232a1fd1f2e90099d74
Jasper Morgal <jpmorgal8@gmail.com>
2023-01-01 00:30:00 -0800

    Updated population for 2023

--- projects/population.js
+++ projects/population.js
@@ -1,20 +1,17 @@
 function updatePop() {
     var today = new Date();
-    var epoch = 1640995200000; // 2022-01-01
-    var popAtEpoch = 7941658000;
-
-    popGrowthOverYear = 66894000;
-
-    var growthRateListed = 0.0084;
-    var growthRateExact = 0.008423178132;
-    var growthRate = growthRateExact;
-
+    var epoch = 1672531200000; // 2023-01-01
+    var popAtEpoch = 8008551995;
+    var popGrowthOverYear = 73518905;
+    // var growthRateListed = 0.0084;
+    // var growthRateExact = 0.008423178132;
+    // var growthRate = growthRateExact;
 
     var timeAfterEpoch = today.getTime() - epoch;
     var timeAfterEpochYears = timeAfterEpoch / (365 * 24 * 60 * 60 * 1000); // change days in year on leap years (2024, 2028...)
 
     var currentPopLinear = popAtEpoch + (timeAfterEpochYears * popGrowthOverYear);
-    var currentPopExp = popAtEpoch * Math.pow(1 + growthRate, timeAfterEpochYears);
+    // var currentPopExp = popAtEpoch * Math.pow(1 + growthRate, timeAfterEpochYears);
     
     var currentPopRounded = Math.round(currentPopLinear);
     var currentPopFormatted = currentPopRounded.toLocaleString();
