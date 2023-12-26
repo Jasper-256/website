eef3da25fd836d1f51edbb2e0132975d6e2baffb
Jasper Morgal <jpmorgal8@gmail.com>
2023-12-26 11:45:27 -0800

    Updated double game

--- games/double.js
+++ games/double.js
@@ -2,22 +2,19 @@ var startingTime = 0;
 var interval1;
 
 function getime() {
-    now = new Date();
+    let now = new Date();
     return now.getTime();
 }
 
 function update() {
-    var currentTime = getime();
-    var timePassed = currentTime - startingTime - 7000;
-    var timeSecs = timePassed / 1000;
-    var dub = Math.pow(2, timeSecs);
-    var round = dub.toFixed(2);
-    var displayNum = "$" + round.toString()
+    let currentTime = getime();
+    let timePassed = currentTime - startingTime - 6644;
+    let timeSecs = timePassed / 1000;
+    let power = Math.pow(2, timeSecs);
+    let round = power.toFixed(2);
+    var displayNum = round.toString();
+    displayNum = "$" + displayNum.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     document.getElementById("amt").innerHTML = displayNum;
-
-    if(round >= 7) {
-        stop();
-    }
 }
 
 function start() {
