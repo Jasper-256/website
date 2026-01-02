7d8be2dff81f5f54345d0d8f09df0bd641c12720
Jasper Morgal <jpmorgal8@gmail.com>
2026-01-02 10:33:12 -0800

    pop data update for 2026

--- projects/population.js
+++ projects/population.js
@@ -1,11 +1,11 @@
 function updatePop() {
     var today = new Date();
-    var epoch = 1735689600000; // 2025-01-01T00:00:00.000Z
-    var popAtEpoch = 8196980849;
-    var popGrowthOverYear = 69264442;
+    var epoch = 1767225600000; // 2026-01-01T00:00:00.000Z
+    var popAtEpoch = 8266245291;
+    var popGrowthOverYear = 68866209;
 
     var timeAfterEpoch = today.getTime() - epoch;
-    var timeAfterEpochFraction = timeAfterEpoch / (365 * 24 * 60 * 60 * 1000); // Fraction of time to next year (2025 is NOT a leap year)
+    var timeAfterEpochFraction = timeAfterEpoch / (365 * 24 * 60 * 60 * 1000); // Fraction of time to next year (2026 is NOT a leap year)
 
     var currentPopLinear = popAtEpoch + (timeAfterEpochFraction * popGrowthOverYear);
     
